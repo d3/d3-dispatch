325848f1613ec99fbc1bd93146652074fea03da2
Mike Bostock <mbostock@gmail.com>
2015-06-06 15:41:29 -0700

    Optimize.

--- src/dispatch.js
+++ src/dispatch.js
@@ -5,56 +5,80 @@ function dispatch() {
 function Dispatch(types) {
   var i = -1,
       n = types.length,
-      typeByName = new Map,
+      callbacksByType = new Map,
+      callbackByName = new Map,
       type,
-      typeName,
       that = this;
 
   that.once = function(type, callback) {
     type = parseType(type);
-    type.type.on(type.name, callback).once = true;
+    that.on(type.name, callback);
+    callbackByName.get(type.name).once = true;
     return that;
   };
 
   that.on = function(type, callback) {
     type = parseType(type);
 
-    // If a type was specified, set or get the callback as appropriate.
-    if (type.type) return arguments.length < 2
-        ? type.type.on(type.name)
-        : (type.type.on(type.name, callback), that);
+    // Return the current callback, if any.
+    if (arguments.length < 2) {
+      return (callback = callbackByName.get(type.name)) && callback.value;
+    }
+
+    // If a type was specified…
+    if (type.type) {
+      var callbacks = callbacksByType.get(type.type),
+          callback0 = callbackByName.get(type.name),
+          i;
+
+      // Remove the current callback, if any, using copy-on-remove.
+      if (callback0) {
+        callback0.value = null;
+        i = callbacks.indexOf(callback0);
+        callbacksByType.set(type.type, callbacks = callbacks.slice(0, i).concat(callbacks.slice(i + 1)));
+        callbackByName.delete(type.name);
+      }
+
+      // Add the new callback, if any.
+      if (callback) {
+        callback = {name: type.name, value: callback, once: false};
+        callbackByName.set(type.name, callback);
+        callbacks.push(callback);
+      }
+    }
 
     // Otherwise, if a null callback was specified, remove all callbacks with the given name.
-    // Otherwise, ignore! Can’t add or return untyped callbacks.
-    if (arguments.length === 2) {
-      if (callback == null) {
-        typeByName.forEach(function(t) {
-          t.on(type.name, null);
-        });
-      }
-      return that;
+    else if (callback == null) {
+      callbacksByType.forEach(function(callbacks, otherType) {
+        if (callback = callbackByName.get(otherType + type.name)) {
+          callback.value = null;
+          var i = callbacks.indexOf(callback);
+          callbacksByType.set(otherType, callbacks.slice(0, i).concat(callbacks.slice(i + 1)));
+          callbackByName.delete(callback.name);
+        }
+      });
     }
+
+    return that;
   };
 
   while (++i < n) {
-    typeName = types[i] + "";
-    if (typeName in that) throw new Error("illegal or duplicate type: " + typeName);
-    type = new Type;
-    typeByName.set(typeName, type);
-    that[typeName] = applier(type);
+    type = types[i] + "";
+    if (type in that) throw new Error("illegal or duplicate type: " + type);
+    callbacksByType.set(type, []);
+    that[type] = applier(type);
   }
 
-  // Extract optional name, e.g., "foo" in "click.foo".
   function parseType(type) {
-    var i = (type += "").indexOf("."), name = "";
-    if (i >= 0) name = type.slice(i + 1), type = type.slice(0, i);
-    if (type && !(type = typeByName.get(type))) throw new Error("unknown type: " + type);
+    var i = (type += "").indexOf("."), name = type;
+    if (i >= 0) type = type.slice(0, i); else name += ".";
+    if (type && !callbacksByType.has(type)) throw new Error("unknown type: " + type);
     return {type: type, name: name};
   }
 
   function applier(type) {
     return function() {
-      var callbacks = type.callbacks, // Defensive reference; copy-on-remove.
+      var callbacks = callbacksByType.get(type), // Defensive reference; copy-on-remove.
           callback,
           callbackValue,
           i = -1,
@@ -62,7 +86,7 @@ function Dispatch(types) {
 
       while (++i < n) {
         if (callbackValue = (callback = callbacks[i]).value) {
-          if (callback.once) type.on(callback.name, null);
+          if (callback.once) that.on(callback.name, null);
           callbackValue.apply(this, arguments);
         }
       }
@@ -73,37 +97,5 @@ function Dispatch(types) {
 }
 
 dispatch.prototype = Dispatch.prototype; // allow instanceof
-
-function Type() {
-  this.callbacks = [];
-  this.callbackByName = new Map;
-}
-
-Type.prototype = {
-  on: function(name, value) {
-    var callback0 = this.callbackByName.get(name),
-        callback,
-        i;
-
-    // Return the current callback, if any.
-    if (arguments.length < 2) return callback0 && callback0.value;
-
-    // Remove the current callback, if any, using copy-on-remove.
-    if (callback0) {
-      callback0.value = null;
-      i = this.callbacks.indexOf(callback0);
-      this.callbacks = this.callbacks.slice(0, i).concat(this.callbacks.slice(i + 1));
-      this.callbackByName.delete(name);
-    }
-
-    // Add the new callback, if any.
-    if (value) {
-      callback = {name: name, value: value, once: false};
-      this.callbackByName.set(name, callback);
-      this.callbacks.push(callback);
-      return callback;
-    }
-  }
-};
 
 export default dispatch;
