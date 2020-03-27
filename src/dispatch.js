82cef38098770c3f183cceb560b254da90997b50
Mike Behnke <mike.behnke@kumanu.com>
2020-03-26 22:01:16 -0400

    fix regex bug in IE where parseTypenames didn't always work properly

--- src/dispatch.js
+++ src/dispatch.js
@@ -13,7 +13,7 @@ function Dispatch(_) {
 }
 
 function parseTypenames(typenames, types) {
-  return typenames.trim().split(/^|\s+/).map(function(t) {
+  return typenames.trim().split(/\s+/).filter(function(t) { return t; }).map(function(t) {
     var name = "", i = t.indexOf(".");
     if (i >= 0) name = t.slice(i + 1), t = t.slice(0, i);
     if (t && !types.hasOwnProperty(t)) throw new Error("unknown type: " + t);
